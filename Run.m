f993212578f2c7d464081af7196366299376ab7b
Nick-Wan-24 <83262628+Nick-Wan-24@users.noreply.github.com>
2021-11-24 16:44:49 +0800

    Add files via upload

---
+++ Run.m
@@ -0,0 +1,64 @@
+% script for 0-9 digits recognize using GMM-HMM model
+% use 39 length MFCC as feature
+% all GMM_HMM model has the same number of states and gaussian model
+% training method: viterbi + updating by EM(for multi-GMM) 
+% or MLE(for single-GMM) + counting(for HMM)
+% continuous pred method: optimal states computation
+
+% loading parameters
+Settings;
+nWords = 10;
+
+
+%% train
+model_all = cell(1, nWords);
+i_word = 0;
+while i_word < nWords
+    feats_train = Util.feature_extract(training_file_directory, para, i_word);
+    model = GMM_HMM(para);
+    model = model.init(feats_train);
+    model = model.train(feats_train);
+    if (~model.check()) % training error: NaN parameter
+        continue; % repeat training
+    end
+    model_all{i_word+1} = model;
+    i_word = i_word + 1; 
+end
+if if_continuous == 1
+    model = GMM_HMM.joint(model_all);
+end
+
+
+%% test
+if if_continuous == 0 % for discretization digits
+    Acc = zeros(1, nWords);
+    for i = 0 : nWords-1
+        feats_test = Util.feature_extract(testing_file_directory, para, i);
+        n_file = length(feats_test);
+        ll = zeros(nWords, n_file);
+        for j = 0 : nWords-1
+            ll(j+1,:) = model_all{j+1}.compute_ll(feats_test);
+        end
+        [~,result] = max(ll);
+        correct = 0;
+        for j = 1 : n_file
+            if result(j)-1 == i
+                correct = correct + 1;
+            end
+        end
+        Acc(i+1) = correct / n_file * 100;
+        disp(['Accuracy of ' num2str(i) ' is ' num2str(Acc(i+1)) '%'])
+    end
+else % for continuous digits
+    feats_test = Util.feature_extract(testing_file_directory, para, '');
+    states = model.viterbi(feats_test);
+    load([testing_file_directory '\number_continuous.mat']); % load correct (11xN)
+    result = cell(1, length(feats_test));
+    Acc = zeros(1, length(feats_test));
+    for i = 1 : length(feats_test)
+        result{i} = GMM_HMM.decode(states{i}, model.nStates, nWords);
+        Acc(i) = Util.distance(result{i}, correct(:,i));
+    end
+    disp(['Average edit distance of continuous audio is ' num2str(mean(Acc))])
+end
+
